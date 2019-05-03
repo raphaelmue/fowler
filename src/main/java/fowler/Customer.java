f285d33a64fd63544da5d80e9850fef9b0492987
Raphael Müßeler <raphael.muesseler@sap.com>
2019-05-03 08:58:54 +0200

    added default case in switch

--- src/main/java/fowler/Customer.java
+++ src/main/java/fowler/Customer.java
@@ -62,6 +62,8 @@ class Customer {
                 if (each.getDaysRented() > 3)
                     thisAmount += (each.getDaysRented() - 3) * 1.5;
                 break;
+            default:
+                throw new IllegalArgumentException("Not allowed here");
         }
         return thisAmount;
     }
