7df6de1f8ae025b7fd98a339bfacecc7a78a8563
Raphael Müßeler <raphael.muesseler@sap.com>
2019-05-07 08:52:04 +0200

    added test

---
+++ src/test/java/fowler/ProgramTest.java
@@ -0,0 +1,43 @@
+package fowler;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class ProgramTest {
+    private static Customer customer;
+    private static Rental firstRental;
+    private static Rental secondRental;
+
+    @BeforeAll
+    public static void initializeTests(){
+        firstRental = new Rental(new Movie("movie1", 1), 10);
+        secondRental = new Rental(new Movie("movie2", 2), 5);
+        customer = new Customer("joe");
+        customer.addRental(firstRental);
+        customer.addRental(secondRental);
+    }
+
+    @Test
+    public void testGetCharge(){
+        Assertions.assertEquals(30, firstRental.getCharge());
+        Assertions.assertEquals(4.5, secondRental.getCharge());
+    }
+
+    @Test
+    public void testGetFrequentRenterPoints(){
+        Assertions.assertEquals(2, firstRental.getFrequentRenterPoints());
+        Assertions.assertEquals(1, secondRental.getFrequentRenterPoints());
+    }
+
+    @Test
+    public void testGetTotalCharge(){
+        Assertions.assertEquals(34.5, customer.getTotalCharge());
+    }
+
+    @Test
+    public void testGetTotalFrequentRenterPoints(){
+        Assertions.assertEquals(3, customer.getTotalFrequentRenterPoints());
+    }
+
+}
